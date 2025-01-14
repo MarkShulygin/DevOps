44346ce0ff8f0f55540c412019eb75d447446148
ubuntu <ubuntu@Ubuntumate.myguest.virtualbox.org>
2025-01-14 20:12:24 +0100

    Updated FuncA for infite series

--- FuncA.cpp
+++ FuncA.cpp
@@ -2,5 +2,9 @@
 #inculde <cmath>
 
 double FuncA::compute(double x) {
-	return M_PI /2 - (M_PI / 2 -x);
+	double result = 0;
+	for (int n = 0; n < 10; ++n) {
+		result += pow(-1,n) * pow(x,2 * n + 1) / (2 * n + 1);
+	}
+	return M_PI / 2 - result;
 }
