097d952b24143cbbe6b7320da4a8289d4ea53042
ubuntu <ubuntu@Ubuntumate.myguest.virtualbox.org>
2025-01-14 20:15:30 +0100

    Hotfix for FuncA

--- FuncA.cpp
+++ FuncA.cpp
@@ -3,7 +3,7 @@
 
 double FuncA::compute(double x) {
 	double result = 0;
-	for (int n = 0; n < 10; ++n) {
+	for (int n = 0; n < 3; ++n) {
 		result += pow(-1,n) * pow(x,2 * n + 1) / (2 * n + 1);
 	}
 	return M_PI / 2 - result;
