62c7777fdc32b02ac181522488c5d75936156d22
qimzlor <50299061+MarkShulygin@users.noreply.github.com>
2025-03-25 19:58:20 +0100

    Update FuncA.cpp

--- FuncA.cpp
+++ FuncA.cpp
@@ -1,13 +1,12 @@
 #include "FuncA.h"
 #include <cmath>
 
-double FuncA::calculate() {
-	double sum = 0.0;
-	double x = 0.5;
-	for (int n = 0; n < 3; ++n) {
-		double numerator = tgamma(2 * n + 1);
-		double denominator = pow(4, n) * pow(tgamma(n + 1), 2) * (2 * n + 1);
-		sum += (numerator / denominator) * pow(x, 2 * n + 1);
-	}
-	return (M_PI / 2) - sum;
-};
+double FuncA::calculate(int N, double x) {
+    double sum = 0.0;
+    for (int n = 0; n < N; ++n) {
+        double num = tgamma(2 * n + 1);
+        double denom = pow(4, n) * pow(tgamma(n + 1), 2) * (2 * n + 1);
+        sum += (num / denom) * pow(x, 2 * n + 1);
+    }
+    return M_PI / 2 - sum;
+}
