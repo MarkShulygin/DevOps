f5229c5e4fc6ea697e8402963e6d959da07af2dd
qimzlor <50299061+MarkShulygin@users.noreply.github.com>
2025-03-25 19:41:39 +0100

    Update FuncA.cpp

--- FuncA.cpp
+++ FuncA.cpp
@@ -3,18 +3,11 @@
 
 double FuncA::calculate() {
 	double sum = 0.0;
-<<<<<<< HEAD
 	double x = 0.5;
 	for (int n = 0; n < 3; ++n) {
 		double numerator = tgamma(2 * n + 1);
 		double denominator = pow(4, n) * pow(tgamma(n + 1), 2) * (2 * n + 1);
 		sum += (numerator / denominator) * pow(x, 2 * n + 1);
-=======
-	for (int n = 0; n < N; ++n) {
-		double numerator = tgamma(2 * n + 1);
-		double denominator = pow(4, n) * pow(tgamma(n + 1), 2) * (2 * n + 1);
-		sum += (numerator / denominator) * pow(x, 2* n + 1);
->>>>>>> branchA
 	}
 	return (M_PI / 2) - sum;
 }
