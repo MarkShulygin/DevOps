8cd15505d4dc79b4ef6b0f8b300aac7885539648
Mark Shulyhin <kram91717@gmail.com>
2025-03-28 18:27:31 +0100

    Creating unit tests

---
+++ tests/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "../FuncA.h"
+
+int main(int argc, char* argv[])
+{
+	FuncA devops;
+
+	FuncA.AddString("The first string");
+
+	if (devops.aString.size() == 1)
+		return 0;
+
+	return 1;
+}
