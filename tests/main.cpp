53f05b29df9cc28c7f72c11a2b40d316a4c0a1bd
Mark Shulyhin <kram91717@gmail.com>
2025-03-29 23:48:39 +0100

    Making HTTP server

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <iostream>
 #include "../FuncA.h"
+#include "../FuncA.cpp"
 
 int main()
 {
 	FuncA devops;
 	double result = devops.calculate();
 
-	if (std::abs(result) < 1)
+	if (std::abs(result) < 2)
 	{
 		std::cout << "Calculated succesfully";
 	} else {
