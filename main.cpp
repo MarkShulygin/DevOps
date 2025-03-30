4770b01912f36f7e32d9ac153c0bbe8e388a78f4
Mark Shulyhin <kram91717@gmail.com>
2025-03-30 15:37:51 +0200

    Update

--- main.cpp
+++ main.cpp
@@ -8,6 +8,6 @@ int main() {
 	FuncA f;
 	int N = 5;
 	double x = 0.5;
-	std::cout << "Result: " << func.calculate() << std::endl;
+	std::cout << "Result: " << f.calculate() << std::endl;
 	return CreateHTTPserver();
 };
--- tests/main.cpp
+++ tests/main.cpp
@@ -7,20 +7,17 @@
 
 int main() {
     FuncA func;
-    const int N = 5;
-    const double x = 0.5;
-    const int count = 10000; // Кол-во вызовов
+    const int count = 27000000;
 
     std::vector<double> values(count);
 
     auto start = std::chrono::high_resolution_clock::now();
 
-    // Генерация значений
-    for (int i = 0; i < count; ++i) {
-        values[i] = func.calculate(N, x);
+    for (int i = 0; i < count; i++) {
+        values[i] = func.calculate();
     }
 
-    // Сортировка
+
     std::sort(values.begin(), values.end());
 
     auto end = std::chrono::high_resolution_clock::now();
@@ -29,12 +26,11 @@ int main() {
     double seconds = elapsed.count();
     std::cout << "Execution time: " << seconds << " seconds\n";
 
-    // Проверка диапазона
     if (seconds >= 5.0 && seconds <= 20.0) {
-        std::cout << "✅ Test passed: time in [5–20] sec\n";
+        std::cout << " Test passed: time in [5-20] sec\n";
         return 0;
     } else {
-        std::cout << "❌ Test failed: time out of range\n";
+        std::cout << " Test failed: time out of range\n";
         return 1;
     }
 }
