587b23c58b37ce11b4633679e0a49a220a096788
qimzlor <50299061+MarkShulygin@users.noreply.github.com>
2025-04-01 23:07:34 +0200

    Create scriptB.sh

---
+++ scriptB.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+SERVER_URL="127.0.0.1/compute"
+
+make_request() {
+    curl -s -X GET "$SERVER_URL" > /dev/null
+    echo "Sending request to the server"
+}
+
+while true; do
+    RANDOM_DELAY=$((RANDOM % 6 + 5))
+    make_request &
+    sleep $RANDOM_DELAY
+done
